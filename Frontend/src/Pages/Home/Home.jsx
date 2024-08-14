d8154128f926674a4921d3fb71ed9d663122d678
Koushal kumar <kushwahakoushal78@gmail.com>
2024-08-14 22:46:39 +0530

    2nd part complited

---
+++ Frontend/src/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Sidebar from '../../Component/Sidebar/Sidebar'
+import MessageContainer from '../../Component/Message/MessageContainer'
+
+function Home() {
+  return (
+    <div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<Sidebar />
+			<MessageContainer />
+		</div>
+
+  )
+}
+
+export default Home
