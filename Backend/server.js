de2b26203d5cddcf1d563d823e42c7805dc75b61
Koushal kumar <kushwahakoushal78@gmail.com>
2024-10-01 21:36:06 +0530

    last commit

--- Backend/server.js
+++ Backend/server.js
@@ -6,11 +6,11 @@ dotenv.config();
 import authRoutes from './Routes/auth.route.js';
 import authmessage from './Routes/message.route.js';
 import userRoute from './Routes/user.route.js';
-
+import path from 'path'
 import connectdb from './Connection/db.js';
 
 const Port = process.env.PORT || 5000
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -18,6 +18,11 @@ app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use('/api/message',authmessage);
 app.use('/api/user',userRoute);
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+app.get("*",(req,resp)=>{
+    resp.sendFile(path.join(__dirname,"/frontend/dist/index.html"))
+})
 
 app.get('/',(req,resp)=>{
     resp.send("hello world")
