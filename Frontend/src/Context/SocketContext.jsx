834605e765530d8668ef14839ff762febcdb19d0
Koushal kumar <kushwahakoushal78@gmail.com>
2024-10-02 09:45:40 +0530

    change url

--- Frontend/src/Context/SocketContext.jsx
+++ Frontend/src/Context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider =({children})=>{
 
     useEffect(()=>{
         if(authUser){
-            const socket = io("http://localhost:5000",{
+            const socket = io("https://chat-app-2-j5fk.onrender.com",{
                 query:{
                     userId:authUser._id,
                 },
